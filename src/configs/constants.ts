9e163731d22c139658222cfbd394067e267682ba
mabbytorres03 <mabbytorres03@gmail.com>
2025-11-09 01:23:38 +0000

    wassup sui

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,3 +1,3 @@
-export const CONTRACTPACKAGEID = "";
-export const CONTRACTMODULENAME = "";
-export const CONTRACTMODULEMETHOD = "";
+export const CONTRACTPACKAGEID = "0x9cf6a373a91d0b835f5c1bf0b6311665b808eea4b702e6b2c8f4643c20f76a0d";
+export const CONTRACTMODULENAME = "nft_marketplace";
+export const CONTRACTMODULEMETHOD = "mint_to_sender";
